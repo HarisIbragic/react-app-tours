20315e02bf6e7da80e995ebda2a989f1151dcd79
Haris Ibragic <ibragicharis01@gmail.com>
2024-04-23 17:37:24 +0200

    create readMore button

--- src/Tour.js
+++ src/Tour.js
@@ -24,10 +24,24 @@
 
 // export default Tour;
 
-import React from 'react';
+import React, { useState } from 'react';
 
-const Tour = () => {
-  return <div>Tour</div>;
+const Tour = ({ id, image, info, name, price, removeTour }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <div className='tour-info'>
+        <span className='tour-price'>${price}</span>
+        <h4>{name}</h4>
+        {readMore ? info : `${info.substring(0, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'show less' : 'read more'}
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Tour;
